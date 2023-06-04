1ec5b39965921b7aeabb8753b5ef38bf0bb6c823
ARTEMSHENDRYK <artemshendryk@gmail.com>
2023-06-04 15:00:12 +0300

    do install

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from "react";
+import { nanoid } from 'nanoid'
+import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import Filter from "./Filter/Filter";
+import css from "./App.module.css";
+
+class App extends React.Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  }
+  
+  handleSubmit = ({ name, number }) => {
+    const { contacts } = this.state;
+    if (contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase()))
+    {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+    this.setState({ contacts: [...contacts, { id: nanoid(), name: name, number: number }] });
+  }
+
+  handleFilter = evt => {
+    this.setState({ filter: evt.target.value });
+  }
+
+  filterContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(({ name }) => name.toLowerCase().includes(filter.toLowerCase()))
+  }
+
+  handleDelete = id => {
+    const { contacts } = this.state;
+    this.setState({ contacts: contacts.filter(contact => contact.id !== id) });
+  }
+
+  render() {  
+    return (
+      <div>
+        <h1 className={css.title}>Phonebook</h1>
+        <ContactForm onAddContact={this.handleSubmit}/>
+        <h2 className={css.title}>Contacts</h2>
+        <Filter
+          filter={this.state.filter}
+          onFilter={this.handleFilter}
+        />
+        {this.state.contacts.length > 0
+          ? (
+            <ContactList
+                contacts={this.filterContacts()} onDelete={this.handleDelete}/>
+          )
+          : (<p className={css.title}>There are no contacts.</p>)    
+        }
+      </div>
+    );
+  };
+}  
+
+export default App;
